0b9cef8921c697aede89cb6da067fe132d1d6f6a
KristinaBychenok <77573069+KristinaBychenok@users.noreply.github.com>
2021-03-16 02:52:23 +0300

    feat: implement task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let open = bracketsConfig.map((item) => item[0]);
+
+    let close = bracketsConfig.map((item) => item[1]);
+
+    let stack = [];
+
+    for (let i = 0; i < str.length; i++) {
+        if (['|', '7', '8'].includes(str[i])) {
+          if (stack.length == 0 || str[i] != stack[stack.length - 1]) stack.push(str[i])
+          else stack.pop()
+        } else if (open.includes(str[i])) {
+            stack.push(str[i])
+        } else {
+            if (stack.length == 0) stack.push(str[i])
+            else {
+                if (close.indexOf(str[i]) == open.indexOf(stack[stack.length - 1])) stack.pop()
+                else stack.push(str[i])
+            }
+        }
+    }
+    return stack.length == 0
 }
